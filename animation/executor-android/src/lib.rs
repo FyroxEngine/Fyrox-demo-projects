5a01d8c13893555746f600fdc35d6080cd3fe4ef
Dmitry Stepanov <d1maxa@yandex.ru>
2026-02-25 17:03:34 +0300

    1.0.0 for animation

--- animation/executor-android/src/lib.rs
+++ animation/executor-android/src/lib.rs
@@ -1,17 +1,17 @@
 //! Android executor with your game connected to it as a plugin.
+use animation::Game;
 use fyrox::{
     core::io, engine::executor::Executor, event_loop::EventLoopBuilder,
     platform::android::EventLoopBuilderExtAndroid,
 };
-use animation::GameConstructor;
 
 #[no_mangle]
 fn android_main(app: fyrox::platform::android::activity::AndroidApp) {
     io::ANDROID_APP
         .set(app.clone())
         .expect("ANDROID_APP cannot be set twice.");
-    let event_loop = EventLoopBuilder::new().with_android_app(app).build();
+    let event_loop = EventLoopBuilder::new().with_android_app(app).build().ok();
     let mut executor = Executor::from_params(event_loop, Default::default());
-    executor.add_plugin_constructor(GameConstructor);
+    executor.add_plugin(Game::default());
     executor.run()
 }
--- animation/executor-wasm/src/lib.rs
+++ animation/executor-wasm/src/lib.rs
@@ -1,60 +1,28 @@
 //! Executor with your game connected to it as a plugin.
-use animation::GameConstructor;
+use animation::Game;
 use fyrox::{
     core::wasm_bindgen::{self, prelude::*},
     dpi::LogicalSize,
-    engine::executor::Executor,
-    engine::GraphicsContextParams,
+    engine::{executor::Executor, GraphicsContextParams},
     event_loop::EventLoop,
     window::WindowAttributes,
 };
 
 #[wasm_bindgen]
-extern "C" {
-    #[wasm_bindgen(js_namespace = console)]
-    fn error(msg: String);
-
-    type Error;
-
-    #[wasm_bindgen(constructor)]
-    fn new() -> Error;
-
-    #[wasm_bindgen(structural, method, getter)]
-    fn stack(error: &Error) -> String;
-}
-
-fn custom_panic_hook(info: &std::panic::PanicInfo) {
-    let mut msg = info.to_string();
-    msg.push_str("\n\nStack:\n\n");
-    let e = Error::new();
-    let stack = e.stack();
-    msg.push_str(&stack);
-    msg.push_str("\n\n");
-    error(msg);
-}
-
-#[inline]
-pub fn set_panic_hook() {
-    use std::sync::Once;
-    static SET_HOOK: Once = Once::new();
-    SET_HOOK.call_once(|| {
-        std::panic::set_hook(Box::new(custom_panic_hook));
-    });
-}
-
-#[wasm_bindgen]
 pub fn main() {
-    set_panic_hook();
     let mut window_attributes = WindowAttributes::default();
     window_attributes.inner_size = Some(LogicalSize::new(1280.0, 720.0).into());
     window_attributes.title = "Animation".to_string();
     let mut executor = Executor::from_params(
-        EventLoop::new().unwrap(),
+        EventLoop::new().ok(),
         GraphicsContextParams {
             window_attributes,
             vsync: false,
+            msaa_sample_count: None,
+            graphics_server_constructor: Default::default(),
+            named_objects: false,
         },
     );
-    executor.add_plugin_constructor(GameConstructor);
+    executor.add_plugin(Game::default());
     executor.run()
 }
